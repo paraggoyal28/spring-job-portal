34e51ba253000c6aa1cc0df6844f02ae3712d8d7
Parag Kumar Goyal <1997parag.goyal@gmail.com>
2024-07-13 19:10:10 +0530

    completed initial project setup, added js, css, html files and created User and UserType data jpa entity

---
+++ jobportal/src/main/java/com/mycode/jobportal/JobportalApplication.java
@@ -0,0 +1,13 @@
+package com.mycode.jobportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobportalApplication.class, args);
+	}
+
+}
